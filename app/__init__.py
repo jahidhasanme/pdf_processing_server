63ef0ce00519d2ab94ddcbb95a6fa9c676ca74e2
Jahid Hasan <jahidsite0@gmail.com>
2025-03-22 03:03:12 +0600

    Refine CORS configuration to restrict origins and methods for enhanced security

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,16 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     
-    CORS(app, origins="*")
+    CORS(app, resources={
+        r"/*": {
+            "origins": [
+                "https://openai4pdf.vercel.app",
+                "http://localhost:3000"
+            ],
+            "methods": ["GET", "POST", "OPTIONS"],
+            "allow_headers": ["Content-Type", "Authorization"]
+        }
+    })
+    
     app.register_blueprint(response_bp)
-
     return app
