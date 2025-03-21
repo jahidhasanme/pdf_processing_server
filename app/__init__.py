851dc9b460a7ef739ad9d271de1a07594ec73209
Jahid Hasan <jahidsite0@gmail.com>
2025-03-22 03:36:23 +0600

    Add additional allowed origin for CORS configuration

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,8 @@ def create_app():
         r"/*": {
             "origins": [
                 "https://openai4pdf.vercel.app",
-                "http://localhost:3000"
+                "http://localhost:3000",
+                "http://python.mswott.net",
             ],
             "methods": ["GET", "POST", "OPTIONS"],
             "allow_headers": ["Content-Type", "Authorization"]
